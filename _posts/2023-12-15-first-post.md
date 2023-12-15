8d6d76a787e2ca803f66296033b017ee06c79a54
Shahid Hussain <shahid.hussain0120@gmail.com>
2023-12-15 11:44:56 +1100

    create my first blog, but need to fix up html, since when i click on blog it doesnt maintain the layout

---
+++ _posts/2023-12-15-first-post.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "My First Post"
+---
+
+# Welcome to My Blog!
+
+Welcome this is my very first posts on my new blog! This blog aims to show my exictement for my new found passion MACHINE LEARNING!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "My First Post",
+        "excerpt":"Welcome to My Blog!   Welcome this is my very first posts on my new blog! This blog aims to show my exictement for my new found passion MACHINE LEARNING!  ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
